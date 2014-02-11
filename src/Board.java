72583f67d7c39a1cf6bbd69ba84d0807ddf39e66
Surangie <Steffen.olaisen@gmail.com>
2014-02-11 18:46:54 +0100

    added some text to board

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,37 @@
 
-public class Board {
+import java.util.*;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.awt.*;
+
+
+public class Board extends JPanel{
+	
 	public Board(){
 		
+		
 	}
+	public class KeyboardListener implements KeyListener{
+		public void keyPressed(KeyEvent e){
+			
+		}
+		public void keyTyped(KeyEvent e){
+			
+		}
+		public void keyReleased(KeyEvent e){
+			
+		}
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		
+	}
+
 	
 	
 
