1881680fbf12356aabdc6a8c800deadb6cf5b01a
Surangie <Steffen.olaisen@gmail.com>
2014-02-11 18:57:28 +0100

    added fram
    For easier debugging underway

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame{
+	public Main(){
+		add(new Board());
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main frame = new Main();
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
