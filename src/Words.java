dc6e86efeaf815470f6d27b1c0a5db67828140e3
Surangie <Steffen.olaisen@gmail.com>
2014-02-11 16:05:15 +0100

    Setting up some framework for the word class

--- src/Words.java
+++ src/Words.java
@@ -1,4 +1,32 @@
 
 public class Words {
+	private int coordinateX;
+	private int coordinateY;
+	private String word;
+	
+	public Words(){
+		
+	}
+	
+	public int getCoordinateX(){
+		return coordinateX;
+	}
+	public void setCoordinateX(int coordinate){
+		this.coordinateX = coordinate;
+	}
+	
+	public int getCoordinateY(){
+		return coordinateY;
+	}
+	public void setCoordinateY(int coordinate){
+		this.coordinateY = coordinate;
+	}
+	
+	public void setWord(){
+		this.word = ""; // skal hente ord fra ord-databasen
+	}
+	public String getWord(){
+		return word;
+	}
 
 }
